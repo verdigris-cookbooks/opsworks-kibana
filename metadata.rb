18e0c7f5bea094335d93e23962769537dafb1221
Andrew Jo <andrewjo@gmail.com>
2015-01-27 14:07:01 -0800

    Changed cookbook name to kibana

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'opsworks_kibana'
+name             'kibana'
 maintainer       'Andrew Jo'
 maintainer_email 'andrew@verdigris.co'
 license          'Simplified BSD'
@@ -6,6 +6,5 @@ description      'Installs and configures Kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'kibana', '>=1.3.0'
-depends 'git', '~3.1.0'
+depends 'kibana_lwrp', '~>2.0'
 depends 'nodejs', '~2.2.0'

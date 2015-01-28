ec9583b641959766d186799b7c2910db8009c325
Andrew Jo <andrewjo@gmail.com>
2015-01-27 22:19:45 -0800

    Fix version constraints

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs and configures Kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'kibana_lwrp', '~>2.0'
+depends 'kibana_lwrp', '~> 2.0'
 depends 'java'
-depends 'nodejs', '~2.2.0'
+depends 'nodejs', '~> 2.2'

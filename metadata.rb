312776210333728176241edce5134aeb41113de9
Andrew Jo <andrewjo@gmail.com>
2015-01-28 11:55:00 -0800

    Switch down to kibana 1.4

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name             'kibana'
+name             'opsworks_kibana'
 maintainer       'Andrew Jo'
 maintainer_email 'andrew@verdigris.co'
 license          'Simplified BSD'
 description      'Installs and configures Kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'kibana_lwrp', '~> 2.0'
+depends 'kibana', '~> 1.4'
 depends 'java'
 depends 'nodejs', '~> 2.2'
